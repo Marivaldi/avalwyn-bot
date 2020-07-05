44d648110b0c26147db6ff5b7c0a22a8d34b2274
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2020-07-05 12:42:49 -0400

    Block non-admins from using !killbot command

--- commands/killbot.js
+++ commands/killbot.js
@@ -3,7 +3,11 @@ const send_message_to_active_channel = require('../helpers/send_message_to_activ
 const bot_store = new Storage('./data/bot_data');
 
 module.exports = async (message, client) => {
-
+        let is_bot_owner = message.author.id === '390639479599136784';
+        if (!is_bot_owner) {
+                message.channel.send("Hey... You're not <@390639479599136784>! Nice try, buddy. :beers:");
+                return;
+        }
         await send_message_to_active_channel("I'm going down for maintenance... hopefully :grimacing:", client)
         client.destroy();
 }
