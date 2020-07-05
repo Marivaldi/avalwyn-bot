2e86f6692948a12fec101d2844009c38a4ae6489
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2020-07-05 12:45:53 -0400

    Use Environment Variables for intervals so that they can be easily updated in live environments

--- scheduled_jobs/setup_scheduled_jobs.js
+++ scheduled_jobs/setup_scheduled_jobs.js
@@ -7,13 +7,34 @@ const AvalwynStorage = require("../AvalwynStorage");
 const random_diplomat = require('./random_diplomat');
 
 module.exports = (client) => {
-    const hourly = cron.schedule("0 */30 * * * *", async () =>  {
+    const resource_interval = process.env.RESOURCE_GENERATION_INTERVAL;
+    const resource_interval_is_not_a_number = isNaN(resource_interval);
+    if(resource_interval_is_not_a_number) {
+        console.log(`Resource Interval: ${resource_interval} is not valid!`);
+        return [];
+    }
+    const resource_generation_schedule = `* 0 */${resource_interval} * * * *`;
+
+    const training_interval = resource_interval * 7;
+    const training_schedule = `* 0 */${training_interval} * * * *`;
+
+    const resource_generation_schedule_is_valid = cron.validate(resource_generation_schedule);
+    const training_schedule_is_valid = cron.validate(training_schedule);
+
+    if(!resource_generation_schedule_is_valid || !training_schedule_is_valid) {
+        console.error("Cron Validation Error!");
+        console.log("Resource Generation Schedule:", resource_generation_schedule);
+        console.log("Training Schedule:", training_schedule);
+        return [];
+    }
+
+    const hourly = cron.schedule(resource_generation_schedule, async () =>  {
         const faction_store = new AvalwynStorage().faction_storage;
         await generate_resources(faction_store, client);
         await decide_battles(faction_store, client);
     });
 
-    const every_three_hours = cron.schedule("0 */210 * * * *", async () =>  {
+    const every_three_hours = cron.schedule(training_schedule, async () =>  {
         const faction_store = new AvalwynStorage().faction_storage;
         await train_skills(faction_store, client);
         await random_diplomat(client);
