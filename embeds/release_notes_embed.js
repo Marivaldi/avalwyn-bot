eb7b6fe8b5506c8ef5a58b8e223903d716d30cbf
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2020-07-06 12:01:59 -0400

    Fixed Fortify Spell command

--- embeds/release_notes_embed.js
+++ embeds/release_notes_embed.js
@@ -4,16 +4,12 @@ const release_notes_version = (process.env.HEROKU_RELEASE_VERSION) ? `(${process
 const embed = new Discord.MessageEmbed()
     .setColor(faction_selection.color)
     .setTitle(`Release Notes ${release_notes_version}`)
-    .setDescription("*Send More* :coffee:");
+    .setDescription("*Where is my* :brain:*?*");
 
 
 embed.addFields({
-    name: ":whiteclaw: Commands Show Correct Prefix :whiteclaw:",
-    value: 'Painstakingly went through and made sure that *most* of the commands you see show the `a!` prefix instead of just `!`.'
-},
-{
-    name: ":mage: Sorted Spells by Level :mage:",
-    value: 'Made sure that all the spells are listed in order by level. Go ahead. Run `a!spells` and see.'
+    name: "Fixed Fortify Spell Command",
+    value: 'I\'m a dingus. Fortify will now say `a!cast fortify [faction]`'
 })
 
 
